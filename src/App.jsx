2dfbf1ed9020e4a5c53f75f062866def88da9d23
damaestro165 <damaestro165@gmail.com>
2023-03-14 12:26:31 +0100

    Added style file

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,11 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className='container is-fluid'>
       <CarForm />
-      <CarList />
       <CarSearch />
+      <CarList />
+
       <CarValue />
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
+import 'bulma/css/bulma.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './App';
-import './index.css';
+import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
